27bc4c09261d540660d77622dc2de359668178d9
sanogueralorenzo <sanogueralorenzo@gmail.com>
2026-02-19 15:18:39 -0800

    Increase setup action bottom margin to 60dp

--- voice/app/src/main/java/com/sanogueralorenzo/voice/setup/SetupUiMetrics.kt
+++ voice/app/src/main/java/com/sanogueralorenzo/voice/setup/SetupUiMetrics.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.unit.dp
 import com.sanogueralorenzo.voice.ui.VoicePillVisualizerWidth
 
 internal val SetupScreenOuterPadding = 20.dp
-internal val SetupScreenBottomPadding = 16.dp
+internal val SetupScreenBottomPadding = 60.dp
 internal val SetupLogoTopOffset = 96.dp
 internal val SetupLogoBottomPadding = 20.dp
 internal val SetupLogoSize = VoicePillVisualizerWidth
