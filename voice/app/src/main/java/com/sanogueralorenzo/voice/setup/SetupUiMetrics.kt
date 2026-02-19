0580f816c8deaf9fce1fce90445e80b5bbabc070
sanogueralorenzo <sanogueralorenzo@gmail.com>
2026-02-19 12:09:43 -0800

    Lower setup splash and intro logo vertical position

--- voice/app/src/main/java/com/sanogueralorenzo/voice/setup/SetupUiMetrics.kt
+++ voice/app/src/main/java/com/sanogueralorenzo/voice/setup/SetupUiMetrics.kt
@@ -4,6 +4,6 @@ import androidx.compose.ui.unit.dp
 import com.sanogueralorenzo.voice.ui.VoicePillVisualizerWidth
 
 internal val SetupScreenOuterPadding = 20.dp
-internal val SetupLogoTopOffset = 32.dp
+internal val SetupLogoTopOffset = 96.dp
 internal val SetupLogoBottomPadding = 20.dp
 internal val SetupLogoSize = VoicePillVisualizerWidth
