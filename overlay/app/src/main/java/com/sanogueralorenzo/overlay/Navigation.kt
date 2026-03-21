68077cf110b798be563930a059ca8f2883d71f51
sanogueralorenzo <sanogueralorenzo@gmail.com>
2026-03-21 00:20:26 -0700

    Consolidate nav routes and extensions into Navigation file

--- overlay/app/src/main/java/com/sanogueralorenzo/overlay/Navigation.kt
+++ overlay/app/src/main/java/com/sanogueralorenzo/overlay/Navigation.kt
@@ -1,7 +1,15 @@
 package com.sanogueralorenzo.overlay
 
+import androidx.navigation.NavController
+
 object NavRoutes {
     const val Home = "home"
     const val Help = "help"
     const val Permissions = "permissions"
+}
+
+fun NavController.navigateSingleTop(route: String) {
+    navigate(route) {
+        launchSingleTop = true
+    }
 }
--- overlay/app/src/main/java/com/sanogueralorenzo/overlay/NavigationExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.sanogueralorenzo.overlay
-
-import androidx.navigation.NavController
-
-fun NavController.navigateSingleTop(route: String) {
-    navigate(route) {
-        launchSingleTop = true
-    }
-}
