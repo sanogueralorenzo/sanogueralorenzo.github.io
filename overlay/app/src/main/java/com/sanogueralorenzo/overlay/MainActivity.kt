77e65b2e30296e8f268a4fe7d23a10bb3155c0ea
sanogueralorenzo <sanogueralorenzo@gmail.com>
2026-03-20 22:27:16 -0700

    overlay: enable edge-to-edge in MainActivity

--- overlay/app/src/main/java/com/sanogueralorenzo/overlay/MainActivity.kt
+++ overlay/app/src/main/java/com/sanogueralorenzo/overlay/MainActivity.kt
@@ -3,12 +3,14 @@ package com.sanogueralorenzo.overlay
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.sanogueralorenzo.overlay.ui.theme.OverlayTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent { OverlayTheme { AppRoot() } }
     }
 }
