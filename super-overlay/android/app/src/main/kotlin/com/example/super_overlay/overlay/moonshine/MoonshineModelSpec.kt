70c20706fd2c47965b09d51d139a1277bac29946
sanogueralorenzo <sanogueralorenzo@gmail.com>
2026-04-10 08:52:02 -0700

    chore: remove super-overlay project

--- super-overlay/android/app/src/main/kotlin/com/example/super_overlay/overlay/moonshine/MoonshineModelSpec.kt
+++
@@ -1,9 +0,0 @@
-package com.example.super_overlay.overlay.moonshine
-
-data class MoonshineModelSpec(
-    val id: String,
-    val fileName: String,
-    val url: String,
-    val sizeBytes: Long,
-    val subdir: String
-)
