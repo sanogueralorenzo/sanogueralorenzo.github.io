901cf4206053185ec66b7372a866777676a0a18e
sanogueralorenzo <sanogueralorenzo@gmail.com>
2026-05-03 00:44:36 -0700

    chore: remove codex app

--- codex/app/lib/l10n/l10n_extensions.dart
+++
@@ -1,6 +0,0 @@
-import 'package:app/l10n/gen/app_localizations.dart';
-import 'package:flutter/widgets.dart';
-
-extension L10nBuildContext on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this)!;
-}
--- codex/app/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:app/src/app.dart';
-import 'package:app/src/di/injector.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  Injector.setup();
-  runApp(const App());
-}
