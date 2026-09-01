7ddf0e782ab5c323a7a6fad034e1487f37ba4aa3
sanogueralorenzo <sanogueralorenzo@gmail.com>
2026-09-01 12:20:26 -0700

    Animate Voice launch splash

--- tools/voice/android/app/src/main/java/com/sanogueralorenzo/voice/MainActivity.kt
+++ tools/voice/android/app/src/main/java/com/sanogueralorenzo/voice/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.sanogueralorenzo.voice
 
+import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,12 +11,32 @@ import com.sanogueralorenzo.voice.ui.theme.VoiceTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashStartedAt = SystemClock.uptimeMillis()
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            keepSplashVisibleUntil(splashStartedAt + SplashMinimumDurationMs)
+        }
         enableEdgeToEdge()
         setContent {
             VoiceTheme {
                 MainNavHost()
             }
         }
+    }
+
+    private fun keepSplashVisibleUntil(endTime: Long) {
+        window.decorView.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    if (SystemClock.uptimeMillis() < endTime) return false
+                    window.decorView.viewTreeObserver.removeOnPreDrawListener(this)
+                    return true
+                }
+            }
+        )
+    }
+
+    private companion object {
+        const val SplashMinimumDurationMs = 850L
     }
 }
